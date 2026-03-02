c38b4a38131a1932e2441c2edca1c84caf9aa5f2
sergantcat <alexander.mylnykov@gmail.com>
2026-03-02 17:37:00 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,35 @@
+require('dotenv').config();
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
+const commands = [
+  new SlashCommandBuilder()
+    .setName('blacklist')
+    .setDescription('Blacklist a user')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to blacklist')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for blacklist')
+        .setRequired(true))
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: '10' })
+  .setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Deploying commands...');
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+    console.log('Commands deployed.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
